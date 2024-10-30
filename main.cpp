ec1a49e5f91c2e3bc0bceaa0d217e44a948a3e9d
Student <1291573216@qq.com>
2024-10-30 22:21:14 +0300

    added greeting Git info

--- main.cpp
+++ main.cpp
@@ -14,3 +14,14 @@ int main(){
    system("pause");
    return 0;
 }
+#include<iostream>
+using namespace std;
+int main(){
+   char<<"Enter your name:";
+   cin>>name;
+   cout<<"Hello,"<<name<<"!\n";
+   cout<<"Nice to see you learning Git\n";
+   system("pause")
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C;
+   return 0;
+}
