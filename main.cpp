940e1fc1c5a65b0d1cab1270eede729f0be9afdc
Student <1291573216@qq.com>
2024-10-30 22:24:43 +0300

    added having a nice day wish

--- main.cpp
+++ main.cpp
@@ -4,3 +4,14 @@ int main(){
    system("pause");
    return 0;
 }
+#include<iostream>
+using namespace std;
+int main(){
+   char name[20];
+   cout<<"Enter your name:";
+   cin>>name;
+   cout<<"Hello,"<<name<<"!\n";
+   cout<<"Nice to see you learning Git\n";
+   system("pause");
+   return 0;
+}
